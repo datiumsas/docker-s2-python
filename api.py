6cb4a1005bd7ebbad2ac6a1751ee76b7706d4f7a
Walid Fajraoui <fajraoui.walid@gmail.com>
2022-11-21 18:11:25 +0100

    add debug message

--- api.py
+++ api.py
@@ -2,4 +2,5 @@ import requests
 import json
 
 response = requests.get("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
+print("hello")
 print(response.json()['init'])
