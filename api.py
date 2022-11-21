3395eb8a22e04633c35a2e2ccbc80fff12f2d935
Walid Fajraoui <fajraoui.walid@gmail.com>
2022-11-21 16:17:18 +0100

    first commit

---
+++ api.py
@@ -0,0 +1,5 @@
+import requests
+import json
+
+response = requests.get("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
+print(response.json()['init'])
